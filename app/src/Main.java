3b2dd70546030f6bdedbee950858f321797cdd90
haderos90 <piekarnia1990@gmail.com>
2023-10-17 19:43:53 +0200

    Zacheta podania liczby

--- app/src/Main.java
+++ app/src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        while (true) {
+            System.out.println("Podaj liczbe do policzenia pierwiastka");
+        }
     }
 }
